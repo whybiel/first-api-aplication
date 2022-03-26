190e82ecc117804849e708c13e0013ac956aadc3
gabrielvnw <gabrimoraes2004@gmail.com>
2022-03-25 23:54:37 -0300

    Primeira aplicação com API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Axios from "axios"
+
+const ApiFilms = Axios.create({
+  baseURL:"https://api.themoviedb.org/3/movie/popular?api_key=28440a19c8a5282c770fccf50ba63ae7&language=pt-BR&page=1"
+})
+
+export default class App extends React.Component{
+
+  state={
+    listFilmes: [""]
+  }
+
+  async componentDidMount(){
+    const response = await ApiFilms.get()
+    console.log(response.data.results)
+
+    const filmes = response.data.results.map((item) => {
+      return{
+        ...item,
+        poster:`https://image.tmdb.org/t/p/w500/${item.poster_path}`
+      }
+    })
+
+    this.setState({
+      listFilmes: filmes
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Filmes</h1>
+        {this.state.listFilmes.map((item) => (
+            <div>
+              <h2>{item.title}</h2>
+              <figure>
+                <img src={item.poster}></img>
+              </figure>
+            </div>
+        ))}
+      </div>
+    )
+  }
 }
-
-export default App;
